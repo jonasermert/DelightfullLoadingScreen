0616bf76377d0c1820e8d1f929fcf27ff42d4a67
ermjonertas <ermert.jonas@yahoo.de>
2024-01-07 14:02:36 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(function() {
+      document.getElementById("loadingScreen").style.display = "none";
+    }, 5000);
+  });
+  
